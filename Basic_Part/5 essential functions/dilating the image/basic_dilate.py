8a4981636a947fb60bcd587167c3542b4e072b93
Abdulkerim Akten <67888040+abdulkerimakten@users.noreply.github.com>
2024-04-04 17:38:24 +0300

    Update basic_dilate.py

--- Basic_Part/5 essential functions/dilating the image/basic_dilate.py
+++ Basic_Part/5 essential functions/dilating the image/basic_dilate.py
@@ -23,6 +23,19 @@ cv.imshow("CANNY EDGES AFTER BLUR", canny_after_blur)
 """Dilation is useful for tasks such as image segmentation, noise removal, and feature extraction."""
 
 dilated = cv.dilate(canny_after_blur, (7,7), iterations=3)
+
+"""
+'kernel = (7, 7)' : This parameter defines the structuring element used for dilation. It's a 7x7 square-shaped structuring element, 
+indicating the neighborhood around each pixel that is considered during the dilation operation.
+
+'iterations=3' : This parameter specifies the number of times dilation is applied. In this case, dilation will be applied three times, 
+which increases the effect of expanding the boundaries of foreground objects.
+
+The function performs dilation by moving the structuring element over the input image. 
+At each position, if at least one pixel under the structuring element is white (255), the output pixel is set to white; otherwise, it remains black (0). 
+This process effectively expands the boundaries of foreground objects, making them larger.
+"""
+
 cv.imshow("Dilated Image", dilated)
 
 
