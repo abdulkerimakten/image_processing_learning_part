e415e1a5321b20f40bf750895e27796c75a40051
Abdulkerim Akten <67888040+abdulkerimakten@users.noreply.github.com>
2024-04-04 13:52:37 +0300

    Update basic_blur.py

--- Basic_Part/5 essential functions/blur/basic_blur.py
+++ Basic_Part/5 essential functions/blur/basic_blur.py
@@ -7,4 +7,10 @@ blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
 cv.imshow("BLURRED IMAGE", blur)
 
 
+"""
+'(3,3)': Specifies the kernel size. Gaussian blur employs a weighted averaging operation based on a Gaussian distribution. 
+Hence, this parameter specifies the size of the Gaussian kernel. Here, using (3,3) creates a 3x3 Gaussian kernel.
+
+'cv.BORDER_DEFAULT': This parameter denotes how borders will be handled. In this case, it represents using the default border pixel values.
+"""
 cv.waitKey(0)
